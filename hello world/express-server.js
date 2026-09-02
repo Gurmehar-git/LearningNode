7611ebab0c8ceead7fbb8b9df256885a0f37f990
Gurmehar-git <gurmehar.nst@gmail.com>
2026-09-02 15:23:20 +0530

    Made express server

--- hello world/express-server.js
+++ hello world/express-server.js
@@ -1,3 +1,12 @@
 const express=require('express');
 const app=express();
 
+app.get("/students/:id",((req,res)=>{
+    const student_id=req.params.id;
+    res.send(`student id is ${student_id}`);
+
+}))
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+}
+);
