011fd883984628dd89e435be38316f611f8eced3
Gurmehar-git <gurmehar.nst@gmail.com>
2026-09-07 15:24:36 +0530

    Jsonpostexample

---
+++ hello world/jsonpostexpress.js
@@ -0,0 +1,16 @@
+
+
+const express = require("express");
+const app = express();
+
+// Parse incoming JSON
+app.use(express.json());
+
+app.post("/echo", (req, res) => {
+  res.json({ received: req.body });
+});
+
+
+app.listen(3000, () => {
+  console.log(`Server running on http://localhost:3000`);
+});
