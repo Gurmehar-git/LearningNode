cf40d221d6955d1e2689b84baf9610b1692a266c
Gurmehar-git <gurmehar.nst@gmail.com>
2026-09-07 09:00:12 +0530

    Created express routes like vid

--- hello world/express-server.js
+++ hello world/express-server.js
@@ -1,15 +1,31 @@
 const express=require('express');
 const app=express();
-app.get("/students",(req,res)=>{
-    res.send(`heyyyyyy`);
+const http=require('http');
 
+app.get("/",(req,res)=>{
+    res.send(`hello world, this is the home page`);
 });
-
-console.log("hello world");
-
-app.get("/students/:id",(req,res)=>{
-    const student_id=req.params.id;
-    res.send(`student id is ${student_id}`);
-
+app.get("/about",(req,res)=>{
+    res.send("This is the about page");
 });
-app.listen(3000)
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+});
+// const myserver=http.createServer(app);
+// myserver.listen((3000),()=>{
+//     console.log("server is running on port 3000");
+// });
+
+
+// app.get("/students",(req,res)=>{
+//     res.send(`heyyyyyy`);
+
+// });
+
+// console.log("hello world");
+
+// app.get("/students/:id",(req,res)=>{
+//     const student_id=req.params.id;
+//     res.send(`student id is ${student_id}`);
+
+// });
